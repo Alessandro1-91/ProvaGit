cb470af455c41386f5a4f202098b05f3e5b03b74
PC <PC@LAPTOP-8347QD2G.homenet.telecomitalia.it>
2020-05-05 09:57:00 +0200

    modificato metodo

---
+++ ProvaGit/src/it/begear/corso/prova_1/Main.java
@@ -0,0 +1,35 @@
+package it.begear.corso.prova_1;
+
+public class Main {
+	
+	private int x; // metri in lunghezza
+	private int y; // metri in larghezza
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	
+	
+		public int area() {
+			return  x*y;
+			
+			}
+	public void nomeStadio(String fgffg) {
+		System.out.println("il nome dello stadio è" + " " + fgffg);
+		
+		
+	}
+	
+
+}
+
